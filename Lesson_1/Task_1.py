378afbf49d894755f2fe3ef6df7800c615afcd2f
azolanda <uboikov@yandex.ru>
2022-10-11 07:24:39 +0300

    Lesson_1 Tasks modified

--- Lesson_1/Task_1.py
+++ Lesson_1/Task_1.py
@@ -6,12 +6,15 @@
 # - 7 -> да
 # - 1 -> нет
 
-weekday = list(range(1, 6))
-num = int(input('Enter day of a week in the range from 1 to 7: '))
+def is_weekend(num):
+    weekday = list(range(1, 6))
+    if num == 6 or num == 7:
+        print('yes')
+    elif num in weekday:
+        print('no')
+    else:
+        print('Error, you num is out of range. Enter correct num in range from 1 to 7.')
 
-if num == 6 or num == 7:
-    print('yes')
-elif num in weekday:
-    print('no')
-else:
-    print('Error, you num is out of range. Enter correct num in range from 1 to 7.')
+
+num_day = int(input('Enter day of a week in the range from 1 to 7: '))
+is_weekend(num_day)
--- Lesson_1/Task_3.py
+++ Lesson_1/Task_3.py
@@ -8,14 +8,19 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-x = int(input("Enter x coords, x do not equal 0: "))
-y = int(input("Enter y coords, y do not equal 0: "))
+def get_quarter_number(x, y):
+    if x > 0 and y > 0:
+        print(f'({x}, {y}): quarter 1')
+    elif x < 0 and y > 0:
+        print(f'({x}, {y}): quarter 2')
+    elif x < 0 and y < 0:
+        print(f'({x}, {y}): quarter 3')
+    elif x > 0 and y < 0:
+        print(f'({x}, {y}): quarter 4')
+    else:
+        print('Error. You should enter numbers is not equal 0')
 
-if x > 0 and y > 0:
-    print(f'({x}, {y}): quarter 1')
-elif x < 0 and y > 0:
-    print(f'({x}, {y}): quarter 2')
-elif x < 0 and y < 0:
-    print(f'({x}, {y}): quarter 3')
-else:
-    print(f'({x}, {y}): quarter 4')
+x_cord = int(input("Enter x coords, x is not equal 0: "))
+y_cord = int(input("Enter y coords, y is not equal 0: "))
+
+get_quarter_number(x_cord, y_cord)
