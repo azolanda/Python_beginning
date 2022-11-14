44c2e3100748a558182cc9d6fd40eb321b42a0c6
azolanda <uboikov@yandex.ru>
2022-11-14 21:57:45 +0300

    Task_1 added

--- Lesson_5/Task_1.py
+++ Lesson_5/Task_1.py
@@ -1,1 +1,15 @@
-# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+from colorama import init, Fore
+
+init(autoreset=True)
+
+text = 'абв зеленый абвход три хр тр трабвбр пр учитьабв'
+
+print(Fore.BLUE + f'Исходный текст:\n--> {text}')
+
+final_text = ' '.join(
+    list(filter(lambda x: not 'абв' in x, list(text.split()))))
+
+print(Fore.GREEN +
+      f'Текст после удаления слов, содержащих "абв":\n--> {final_text}')
